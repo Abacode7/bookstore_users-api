df28ae323238d029213a36816254d3e8aa3a997a
Abraham Olamipo <aolamipo@teamapt.com>
2021-01-06 01:20:38 +0100

    addition: included support for environment variables from file

--- app/application.go
+++ app/application.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Abacode7/bookstore_users-api/domain/users"
 	"github.com/Abacode7/bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 )
@@ -14,19 +15,25 @@ import (
 var router = gin.Default()
 
 func StartApplication() {
+	/// Load .env config file into the os
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatalln(err)
+	}
 
-	dbUser := os.Getenv("DBUSER")
-	dbPassword := os.Getenv("DBPASSWORD")
-	dbHost := os.Getenv("DBHOST")
-	dbPort := os.Getenv("DBPORT")
-	dbName := os.Getenv("DBNAME")
+	/// Get environment variables from os
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
 	/// Generates connections string and opens the connection
 	/// using the provided data source
 	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
-	db, err := mysql.Init(dataSource)
-	if err != nil {
-		log.Fatal(err)
+	db, sqlErr := mysql.Init(dataSource)
+	if sqlErr != nil {
+		log.Fatalln(sqlErr)
 	}
 
 	/// Factory and DI: Initializes all applications layers
